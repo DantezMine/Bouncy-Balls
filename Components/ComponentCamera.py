fda18dda64c4d600a246eb67c0ae58d90f34ed8b
oskideboski <oskieloch@gmx.ch>
2024-05-03 23:13:42 +0200

    cameruh movement

--- Components/ComponentCamera.py
+++ Components/ComponentCamera.py
@@ -6,23 +6,26 @@ import math
 import math
 
 class Camera(Component.Component):
-    def __init__(self, position = Vec2(0,0), scale = 1, boundLen = Vec2(5,5)):
-    def __init__(self, position = Vec2(0,0), scale = 1, boundLen = Vec2(5,5)):
+    def __init__(self, position = Vec2(0,0), scale = 1, boundLen = None):
         self.name = Component.ComponentType.Camera
         self.parent = None
         self.initPos = position
         self.scale = scale #factor by which the scene scales, or zoom factor ; greater -> more zoomed in ; screen length = 2/scale
-        self.boundLen = boundLen
-    
-        self.scale = scale #factor by which the scene scales, or zoom factor ; greater -> more zoomed in ; screen length = 2/scale
-        self.boundLen = boundLen
+        self.minimalBound = Vec2(2/scale,2/scale)
+        self.boundLen = self.minimalBound if boundLen is None else boundLen
+        
+        
+    def Start(self):
+        self.parent.GetComponent(ComponentType.Transform).position = self.initPos
         
     def Update(self, deltaTime):
-        self.scene = self.parent.GetParentScene()
-        balls = self.scene.GetObjectsWithComponent(ComponentType.Ball)
-        for ball in balls:
-            ballPosition = ball.GetComponent(ComponentType.Transform).position
-        self.MoveCamera(ballPosition)
+        if self.boundLen.x >= self.minimalBound.x:
+            if self.boundLen.y >= self.minimalBound.y:
+                self.scene = self.parent.GetParentScene()
+                balls = self.scene.GetObjectsWithComponent(ComponentType.Ball)
+                for ball in balls:
+                    ballPosition = ball.GetComponent(ComponentType.Transform).position
+                self.MoveCamera(ballPosition)
         
     def EnforceBounds(self):
         def sign(x):
@@ -48,24 +51,3 @@ class Camera(Component.Component):
         super().Decode(obj)
         self.scale = obj["scale"]
         self.initPos = Vec2.FromList(obj["initPos"])
-        
-        
-    def EnforceBounds(self):
-        def sign(x):
-            if x == 0:
-                return 0
-            return -1 if x < 0 else 1
-         
-        transform = self.parent.GetComponent(ComponentType.Transform)
-        signedDist = Vec2(1,1)/self.scale - self.boundLen/2.0 + abs(transform.position)
-        signedDist.x = max(0,signedDist.x) * sign(transform.position.x)
-        signedDist.y = max(0,signedDist.y) * sign(transform.position.y)
-        transform.position -= signedDist
-        
-    def MoveCamera(self, position):
-        self.parent.GetComponent(ComponentType.Transform).position = position
-        self.EnforceBounds()
-        
-    def ScaleCamera(self, scale):
-        self.scale = scale
-        self.EnforceBounds()
